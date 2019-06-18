85f3b11ac929c3a6f15d6c67df775e80e36441ea
Max <maksymilianrozanski@gmail.com>
2019-06-18 11:35:16 +0200

    fix typo in class name

--- src/main/kotlin/io/github/maksymilianrozanski/demo/RealEstateApplication.kt
+++ src/main/kotlin/io/github/maksymilianrozanski/demo/RealEstateApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class RealestateApplication
+class RealEstateApplication
 
 fun main(args: Array<String>) {
-    runApplication<RealestateApplication>(*args)
+    runApplication<RealEstateApplication>(*args)
 }
--- src/test/kotlin/io/github/maksymilianrozanski/demo/RealEstateApplicationTests.kt
+++ src/test/kotlin/io/github/maksymilianrozanski/demo/RealEstateApplicationTests.kt
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class RealestateApplicationTests {
+class RealEstateApplicationTests {
 
     @Test
     fun contextLoads() {
