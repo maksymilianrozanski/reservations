f7137144b168288f38753b67a340c710b8630335
Max <maksymilianrozanski@gmail.com>
2019-06-18 11:23:59 +0200

    create project, add vcs

---
+++ src/main/kotlin/io/github/maksymilianrozanski/demo/RealestateApplication.kt
@@ -0,0 +1,11 @@
+package io.github.maksymilianrozanski.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RealestateApplication
+
+fun main(args: Array<String>) {
+    runApplication<RealestateApplication>(*args)
+}
