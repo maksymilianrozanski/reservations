5a7a7a2d772633e3a36b787d92f17bdf4855d922
Max <maksymilianrozanski@gmail.com>
2019-06-20 16:33:03 +0200

    refactor test code

--- src/test/kotlin/io/github/maksymilianrozanski/demo/service/TestTableServiceImplTest.kt
+++ src/test/kotlin/io/github/maksymilianrozanski/demo/service/TestTableServiceImplTest.kt
@@ -3,34 +3,39 @@ package io.github.maksymilianrozanski.demo.service
 import com.nhaarman.mockitokotlin2.argThat
 import io.github.maksymilianrozanski.demo.dao.TestTableRepository
 import io.github.maksymilianrozanski.demo.entity.Reservations
+import org.junit.Before
 import org.junit.Test
+import org.mockito.Mock
 import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
 import java.sql.Timestamp
 
 internal class TestTableServiceImplTest {
 
+    lateinit var service: TestTableServiceImpl
+    @Mock
+    lateinit var repositoryMock: TestTableRepository
+
+    @Before
+    fun before() {
+        MockitoAnnotations.initMocks(this)
+        service = TestTableServiceImpl()
+        service.repository = repositoryMock
+    }
+
     @Test
     fun findAll() {
-        val service = TestTableServiceImpl()
-        val repositoryMock = Mockito.mock(TestTableRepository::class.java)
-        service.repository = repositoryMock
-
         service.findAll()
         Mockito.verify(repositoryMock).findAll()
     }
 
     @Test
     fun addReservation() {
-        val service = TestTableServiceImpl()
-        val repositoryMock = Mockito.mock(TestTableRepository::class.java)
-        service.repository = repositoryMock
-
         val title = "Example title"
         val description = "Example description"
         val start = Timestamp(1561037749627L)
         val end = Timestamp(1561037763719L)
         service.addReservation(title, description, start, end)
-
         Mockito.verify(repositoryMock).save(argThat<Reservations> {
             this.title == title && this.description == description && this.start == start && this.end == end && this.user == ""
         })
