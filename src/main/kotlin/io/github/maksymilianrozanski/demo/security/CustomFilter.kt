75e80b070f5711d0b9231b0bb0873e02549c5dfb
Max <maksymilianrozanski@gmail.com>
2019-09-04 18:30:32 +0200

    add PUT method to CustomFilter

--- src/main/kotlin/io/github/maksymilianrozanski/demo/security/CustomFilter.kt
+++ src/main/kotlin/io/github/maksymilianrozanski/demo/security/CustomFilter.kt
@@ -13,7 +13,7 @@ class CustomFilter : OncePerRequestFilter() {
     @Throws(ServletException::class, IOException::class)
     override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, chain: FilterChain) {
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200")
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE")
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE")
         response.setHeader("Access-Control-Allow-Headers",
                 "authorization, content-type, xsrf-token, Cache-Control, remember-me, WWW-Authenticate")
         response.addHeader("Access-Control-Expose-Headers", "xsrf-token")
